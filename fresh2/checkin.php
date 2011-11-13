4a7a59e077be7e0b468882346b20b60fd66f7e89
pmsinghin <pmsinghin@gmail.com>
2011-11-14 01:46:26 +0530

    Update fresh2/checkin.php

--- fresh2/checkin.php
+++ fresh2/checkin.php
@@ -2,12 +2,63 @@
 require_once("config.php");
 require_once("GPlaceAPI.php");
 $place = new GPlaceAPI();
+
+header('Content-type: text/html; charset=utf-8');
 ?>
-<?php header('Content-type: text/html; charset=utf-8'); ?>
 <!DOCTYPE html PUBLIC "-//WAPFORUM//DTD XHTML Mobile 1.0//EN" "http://www.wapforum.org/DTD/xhtml-mobile10.dtd">
 <html>
     <head>
         <?php require_once("css.php"); ?>
+        <!-- Included online JQuery library and to avoid local copy, to use its AJAX functionality -->
+        <script type="text/javascript" src="http://code.jquery.com/jquery-1.4.2.min.js"></script>
+        <script type="text/javascript">
+            // Function defined here to show message board updates thru AJAX
+            function fn_show_msgboard(){
+                // Make an AJAX call.
+                form_elements = $("#frm_message_board").serialize();
+                $.ajax({
+                    type: "POST",
+                    data: "q=showmsgs&"+form_elements,
+                    url: "messageboard.php",
+                    async: true,
+                    success: function(msg){
+                        $("#div_messages").attr("innerHTML", msg);
+                    }
+                });
+            }
+            
+            // Function defined here to show message board updates thru AJAX
+            function fn_post_message(){
+                // Check, if there is no text entered in the msg_content, don't do anything.
+                if( $("#msg_content").val().trim() == "" ){
+                    return false;
+                }
+                
+                // Else make an AJAX call and post the message.
+                form_elements = $("#frm_message_board").serialize();
+                $.ajax({
+                    type: "POST",
+                    data: "q=post_message&"+form_elements,
+                    url: "messageboard.php",
+                    async: false,
+                    success: function(msg){
+                        fn_show_msgboard();
+                    }
+                });
+            }
+            
+            // This is on ready call to function fn_show_msgboard(), to start showing messageboard updates automatically.
+            // The frequency of refreshing the messageboard is kept as 5 seconds.
+            $(document).ready(
+                function(){
+                    // Call to this function for first time update.
+                    fn_show_msgboard();
+                    
+                    // Messages refresh function.
+                    setInterval("fn_show_msgboard();",3000);
+                }
+            );
+        </script>
     </head>
     <body>
         <?php
@@ -40,30 +91,28 @@ $place = new GPlaceAPI();
         if (isset($_GET['lng'])) {
             $lng = $_GET['lng'];
         }
-        $query = sprintf("Select * from places where place_id='%s'", mysql_real_escape_string($placeId));
-        $result = mysql_query($query);
-        if (!$result) {
-            $message = 'Could not get place detail!' . $query;
-            die($message);
-        }
+
         $freshPlaceId = 0;
         $isOldPlace = false;
-        if ($row = mysql_fetch_assoc($result)) {
-            $freshPlaceId = $row['fresh_place_id'];
-            $isOldPlace = true;
-        } else {
-            $query = sprintf("Insert into places(place_id,source,place_name,lat,lng,place_ref) values ('%s','G','%s',%s,%s,'%s') ", $placeId, mysql_real_escape_string($placeName), $lat, $lng, mysql_real_escape_string($placeReference));
+
+        $placeDetails = getPlaceDetails( $placeId );
+        if( $placeDetails == 0 ){
+            $query = sprintf("INSERT INTO places(place_id,source,place_name,lat,lng,place_ref) values ('%s','G','%s',%s,%s,'%s') ", $placeId, mysql_real_escape_string($placeName), $lat, $lng, mysql_real_escape_string($placeReference));
             $result = mysql_query($query);
             $freshPlaceId = mysql_insert_id();
         }
-        $query = sprintf("Insert into checkin(fresh_place_id,person_id) values (%s,'%s')", $freshPlaceId, $userId);
-        $result = mysql_query($query);
+        else {
+            $freshPlaceId = $placeDetails['fresh_place_id'];
+            $isOldPlace = true;
+        }
 
+        $query = sprintf("INSERT INTO checkin(fresh_place_id,person_id) VALUES(%s,'%s')", $freshPlaceId, $userId);
+        $result = mysql_query($query);
 
         // Check result
         if ($isOldPlace) {
             //Place already existed
-            $query = sprintf("SELECT place_name,vicinity,phone,address,rating FROM place_detail where place_id='%s' ", $placeId);
+            $query = sprintf("SELECT place_name,vicinity,phone,address,rating FROM place_detail WHERE place_id='%s' ", $placeId);
             // Perform Query
             $result = mysql_query($query);
             // Check result
@@ -76,14 +125,16 @@ $place = new GPlaceAPI();
             // One of the mysql result functions must be used
             if ($row = mysql_fetch_assoc($result)) {
                 echo "You are now Checked Into: ";
-                echo $row['place_name'] . "!";
+                $locationName = $row['place_name'];
+                echo $locationName . "!";
                 // commenting these results out because now we are going to focus on messages, but they were originally there to enable confirmation of successful check-in and access to Gplaces
                 // echo "<br />Vicinity: " . $row['vicinity'];
                 // echo "<br />Phone: " . $row['phone'];
                 // echo "<br />Address: " . $row['address'];
                 // echo "<br />Rating: " . $row['rating'];
             }
-        } else {
+        }
+        else {
             // commenting out text like phone number etc, to focus on messaging, but they were there for debugging
             echo "You are now Checked Into: ";
             $businessDetail = $place->getBusinessDetail($placeReference);
@@ -107,57 +158,38 @@ $place = new GPlaceAPI();
                 // echo "<br />Rating: " . $result['rating'];
                 $rating = mysql_real_escape_string($result['rating']);
             }
-            $query = sprintf("Insert into place_detail(fresh_place_id,place_id,place_name,lat,lng,place_ref,vicinity,phone,address,rating) values (%s,'%s','%s',%s,%s,'%s','%s','%s','%s',%s) ", $freshPlaceId, $placeId, $locationName, $lat, $lng, mysql_real_escape_string($placeReference), $vicinity, $phone, $address, $rating);
+            $query = sprintf("INSERT INTO place_detail(fresh_place_id,place_id,place_name,lat,lng,place_ref,vicinity,phone,address,rating) values (%s,'%s','%s',%s,%s,'%s','%s','%s','%s',%s) ", $freshPlaceId, $placeId, $locationName, $lat, $lng, mysql_real_escape_string($placeReference), $vicinity, $phone, $address, $rating);
             $result = mysql_query($query);
             //echo $query;
         }
+
+        function getPlaceDetails( $prm_placeId ){
+            // Perform Query and get fresh_place_id
+            $query = "SELECT * FROM places WHERE place_id = '$prm_placeId'";
+            $result = mysql_query($query);
+            $row = mysql_fetch_assoc($result);
+            if (mysql_num_rows($result) == 0) {
+                return 0;
+            }
+            return $row;
+        }
+
         ?>
+
         <!-- Now we let people leave and read messages -->
         Leave a message!
-        <form action="messageboard.php" method="post">
+        <form id="frm_message_board" method="POST">
             <input type="hidden" name="user_id" id="user_id" value="<?php echo $userId; ?>" />
             <input type="hidden" name="place_id" id="place_id" value="<?php echo $placeId; ?>" />
-            <input type="hidden" name="q" id="q" value="post_message" />
             <br />
             <input type="text" name="msg_content" id="msg_content" />
-            <input type="submit" value="Yodel It!" />
+            <input type="button" value="Yodel It!" onclick="fn_post_message();" />
         </form>
 
         <!-- Read messages -->
         <br>
-        'Da Message Board:
+        'Da <?php echo $locationName; ?> Message Board:
         <br>
-
-        <!-- NOTE TO PROGRAMMER: THIS IS WHERE THINGS STOP WORKING. IF YOU DELETE THE REST OF THE CODE ON THIS PAGE, IT WORKS FINE -->
-
-<?php
-// SQL QUERY TO RETREIVE MESSAGES, AND RETREIVE THE FIRST NAME OF THE AUTHOR OF EACH MESSAGE
-$query = sprintf("SELECT messages.date_and_time,messages.msg_content,person.first_name FROM messages LEFT JOIN person ON (person.person_id = messages.person_id) WHERE messages.place_id='%s' ", mysql_real_escape_string($placeId));
-// Perform Query
-$result = mysql_query($query);
-// Check result
-// This shows the actual query sent to MySQL, and the error. Useful for debugging.
-if (!$result) {
-    $message = 'Zero Messages';
-    die($message);
-}
-// Fetch the messages from mysql and echo in a table
-echo '<table>';
-$count = 1;
-while ($row = mysql_fetch_assoc($result)) {
-    echo '<tr>';
-    # Added Date formatting while echoing the date.
-    echo "<td><b>" . $row['first_name'] . "</b>" . " (" . date('l H:i:s A', intval($row['date_and_time'])) . ") : " . $row['msg_content']."</td>";
-    echo '</tr>';
-    if ($count++ > 20)
-        break;
-}
-echo '</table>';
-//		else{
-//			die("Sorry, we had an error accessing rows for this place in the MySQL database.");
-//		}
-?>
-
-
+        <div id="div_messages"></div>
     </body>
 </html>
