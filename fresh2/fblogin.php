c09597ac9beb4146c606313925e9b1ebfc7b69c3
RockingChair LLC <rockingchairllc@gmail.com>
2011-11-14 22:44:14 -0500

    fb fix

--- fresh2/fblogin.php
+++ fresh2/fblogin.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: http://www.facebook.com/dialog/oauth?client_id=222936987739560&redirect_uri=http://ec2-174-129-1-223.compute-1.amazonaws.com&display=touch&scope=manage_pages&response_type=token");
-//header("https://www.facebook.com/dialog/oauth?client_id=222936987739560&redirect_uri=http://ec2-174-129-1-223.compute-1.amazonaws.com&scope=manage_pages&response_type=token");
+header("Location: http://www.facebook.com/dialog/oauth?client_id=222936987739560&redirect_uri=http://ec2-184-73-103-153.compute-1.amazonaws.com/FreshDevFork/fresh2/&display=touch&scope=manage_pages&response_type=token");
+//header("https://www.facebook.com/dialog/oauth?client_id=222936987739560&redirect_uri=http://ec2-184-73-103-153.compute-1.amazonaws.com/FreshDevFork/fresh2/&scope=manage_pages&response_type=token");
 ?>
